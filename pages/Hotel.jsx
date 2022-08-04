6f445e431bf499565941eb8491fe19ad0ba78b55
BENJY33X <abdoycode@gmail.com>
2022-08-04 11:10:52 +0100

    page hotel for you opinio click hotel

---
+++ pages/Hotel.jsx
@@ -0,0 +1,335 @@
+import { Col, Container, Grid, Image, Modal, red, Row, Spacer, Text } from "@nextui-org/react"
+import { useEffect, useState } from "react";
+import React from "react";
+import { BiChevronLeft, BiChevronRight } from "react-icons/bi";
+import { FaChevronDown } from "react-icons/fa";
+const Hotel = () => {
+    const data = {
+        name: "Menara Palace",
+        address: "",
+        city: "",
+        etoiles: 3,
+        services: [
+            "Wi-Fi",
+            "Parking",
+            "Restaurant",
+            "Bar",
+            "Spa",
+            "Pool",
+            "Gym",
+            "Laundry"
+        ],
+        url: {
+            photo1: "https://smartbooking.ma/smart/web/uploads/24/IDOU-ANFA-HOTEL-&-SPA-24-xo.jpg",
+            photo2: "https://content.r9cdn.net/himg/d2/af/97/hotelsdotcom-255339-16b44f8b_w-334339.jpg",
+            photo3: "https://nypost.com/wp-content/uploads/sites/2/2022/02/Hotel-feature.jpg?quality=75&strip=all",
+            photo4: "https://static11.com-hotel.com/uploads/hotel/317641/photo/hotel-relax-marrakech_15970825071.jpg",
+        },
+        email: "",
+        description: "",
+        price: "",
+        datadepart: "07/03/2023",
+        dataDarivee: "10/03/2023",
+        chambreType: [
+            { Type: "chambre Single", Prix: "1400", device: "MAD" },
+            { Type: "chambre Double", Prix: "1800", device: "MAD" },
+            { Type: "chambre junio", Prix: "2000", device: "MAD" }
+        ]
+    };
+    const [next, setNext] = useState(true);
+    const [prev, setPrev] = useState(true);
+    const [visible, setVisible] = React.useState(false);
+    const [hotel, setHotel] = useState({ data });
+    const [numbre, setNumbre] = useState(1);
+    const [model, setModel] = useState(null);
+    const affechModel = (imagSRC) => {
+        setVisible(true);
+        setModel(imagSRC);
+    }
+    useEffect(() => {
+        setModel(hotel.data.url[`photo${numbre}`]);
+        setNext(numbre >= Object.keys(hotel.data.url).length ? false : true);
+        setPrev(numbre <= 1 ? false : true);
+    }, [numbre]);
+    const suiverImage = async (type) => {
+        if (type === "next") {
+            setNumbre(numbre => numbre + 1);
+        } else {
+            setNumbre(numbre - 1);
+        }
+    }
+    const closeHandler = () => {
+        setVisible(false);
+    };
+    var count = (Object.keys(hotel.data.url).length) - 3;
+
+    return (
+        <Container
+            css={{
+                width: "100%",
+                margin: "0",
+                maxWidth: "100% !important",
+                '@sm': { padding: "60px", }
+            }}
+        >
+            {/* 6-Line Background Decoration */}
+            <div className="bgLines">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+
+
+            {/* MAIN */}
+            <Row>
+                {/* Partie Daroite*/}
+                <Grid.Container>
+                    <Grid xs={12} sm={6}>
+                        <Col gap={14}>
+                            <Col>
+                                <h1>Hotel {hotel.data.name}</h1>
+                                <Text color="Red" h3>{hotel.data.etoiles} étoiles</Text>
+                            </Col>
+                            {/* image global */}
+                            <Col css={{
+                                cursor: "pointer"
+                            }}
+                            >
+                                <Image
+                                    src={hotel.data.url.photo1}
+                                    alt=""
+                                    height="340px"
+                                    width="100%"
+                                    objectFit="cover"
+                                    onClick={() => affechModel(hotel.data.url.photo1)}
+                                />
+                                <Spacer y={1 / 3} />
+                            </Col>
+                            {/* images */}
+                            <Row>
+                                <Grid.Container >
+                                    <Grid xs={12} md={6} css={{ cursor: "pointer" }} >
+                                        <Image
+                                            src={hotel.data.url.photo2}
+                                            alt=""
+                                            height="340px"
+                                            width="100%"
+                                            objectFit="cover"
+                                            onClick={() => { affechModel(hotel.data.url.photo2) }}
+                                        />
+                                    </Grid>
+                                    <Grid xs={12} md={6} css={{ cursor: "pointer" }} onClick={() => { affechModel(hotel.data.url.photo3) }}>
+                                        <Col css={{ position: "relative", }}>
+                                            <Image
+                                                src={hotel.data.url["photo3"]}
+                                                alt=""
+                                                height="340px"
+                                                width="100%"
+                                                objectFit="cover"
+                                            />
+                                            <Row css={{ backgroundColor: "rgba(172, 77, 77, 0.425)", width: "100%", height: "100%", position: "absolute", top: "0" }}>
+                                                <Text zIndex="90">
+                                                </Text>
+                                            </Row>
+                                            <Container zIndex="100" css={{ position: "absolute", top: "5%", width: "100%", maxWidth: "100%", height: "100%" }}>
+                                                <Row css={{ height: "100%" }} justify="center" align="center">
+                                                    <Text h1 size={60} color="$white">
+                                                        +{count}
+                                                    </Text>
+                                                </Row>
+                                            </Container>
+                                        </Col>
+                                    </Grid>
+                                </Grid.Container>
+                            </Row>
+                            {/* description */}
+                            <Spacer y={2} />
+                            <Col>
+                                <Text>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia, animi natus eius maxime, voluptatem impedit ipsam accusantium aspernatur voluptates obcaecati quos possimus autem qui fugiat provident eaque repellendus praesentium ea ut? Totam doloremque eius sint.
+                                    <br />
+                                    <br />
+                                    id est soluta necessitatibus reiciendis explicabo ipsum? Odit libero placeat reiciendis ut? Eveniet itaque iure, veniam minus deleniti fugiat ducimus sed rerum expedita suscipit ipsam laboriosam consectetur. Minima ipsum quidem numquam iusto! Repellendus, aspernatur pariatur? Quia nesciunt natus ut doloribus nulla iure magni eum optio consequ reiciendis explicabo ipsum? Odit libero placeat reiciendis ut? Eveniet itaque iure, veniam minus deleniti fugiat ducimus ssuscipit ipsam laboriosam consectetur. Minima ipsum quidem numquam iusto! Repellendus, aspernatur pariatur? Quia nesciunt natus ut doloribus nulla iure magni eum optioed rerum expedita suscipit ipsam .
+                                    <br />
+                                    <br />
+                                    laboriosam consectetur. Minima ipsum quidem numquam iusto! Repellendus, aspernatur pariatur? Quia nesciunt natus ut doloribus nulla iure magni eum optio consequ reiciendis explicabo ipsum? Odit libero placeat reiciendis ut? Eveniet itaque iure, veniam minus deleniti fugiat ducimus sed rerum expedita  consequatur eos ducimus suscipit distinctio iusto rem nam quidem, minima, dolores enim, illum dolorem? Sapiente recusandae iusto voluptatibus eius magni.
+                                </Text>
+                                <Spacer y={2} />
+                            </Col>
+                            {/* services */}
+                            <Col css={{
+                                background: "rgb(179, 48, 48)",
+                                width: "60%",
+                                padding: "20px 16px",
+                                color: "rgb(243, 243, 243)"
+                            }}>
+                                <h2 >services</h2>
+                                <ul>
+                                    {hotel.data.services.map((service, index) => (
+                                        <li key={index}>{service}</li>
+                                    ))}
+                                </ul>
+                            </Col>
+                        </Col>
+                        <Spacer x={2 / 3} />
+                    </Grid>
+
+                    {/* partie Gauche */}
+                    <Grid xs={12} sm={6}>
+                        <Col css={{
+                            border: "2px solid gray",
+                            height: "600px"
+                            , '@md': {
+                                width: "100%",
+                            }
+                        }}>
+                            {/* header de table */}
+                            <Row css={{
+                                borderBottom: "2px solid gray",
+                                height: "20%"
+                            }} >
+                                {/* date d'arivee */}
+                                <Col css={{
+                                    height: "100%",
+                                    borderRight: "solid 2px gray"
+                                }}>
+                                    <Grid.Container>
+                                        <Spacer x={2} />
+                                        <Grid xs={12}>
+                                            <Text h4 >
+                                                Date d'araiver
+                                            </Text>
+                                            <Spacer y={2} />
+                                        </Grid>
+                                        <Grid xs={4}>
+                                            <Text>
+                                                {hotel.data.dataDarivee}
+                                            </Text>
+                                        </Grid>
+                                    </Grid.Container>
+                                </Col>
+                                {/* date de depart  */}
+                                <Col css={{
+                                    height: "100%",
+
+                                }}>
+                                    <Grid.Container>
+                                        <Spacer x={2} />
+                                        <Grid xs={12}>
+                                            <Text h4 >
+                                                Date de depart
+                                            </Text>
+                                            <Spacer y={2} />
+                                        </Grid>
+                                        <Grid xs={4}>
+                                            <Text>
+                                                {hotel.data.dataDarivee}
+                                            </Text>
+                                        </Grid>
+                                    </Grid.Container>
+                                </Col>
+                            </Row>
+                            {/* affichage les information des chambres de hotel */}
+                            <Col css={{ borderBottom: "gray solid 2px" }}>
+                                {hotel.data.chambreType.map((chambre, index) => (
+                                    <Row css={{ height: "100px" }}>
+                                        <Spacer y={2} />
+                                        <Grid.Container>
+                                            <Spacer x={2} />
+                                            <Row>
+                                                <Grid xs={12}>
+                                                    <Text>{chambre.Type}
+                                                        <br />
+                                                        <br />
+                                                        <Text color="red" >
+                                                            Prix {chambre.Prix} {chambre.device}
+                                                        </Text>
+                                                    </Text>
+                                                    <Spacer y={2} />
+                                                </Grid>
+                                                <Grid>
+                                                    <Row>
+                                                        <Text color="red">2</Text>
+                                                        <Spacer x={1} />
+                                                        <FaChevronDown />
+                                                        <Spacer x={1} />
+                                                    </Row>
+                                                </Grid>
+                                            </Row>
+                                        </Grid.Container>
+                                    </Row>
+                                ))}
+                            </Col>
+
+                            {/* calcule total de prix de 3 joour */}
+                            <Text>
+                                <Row>
+                                    <Col css={{ padding: "14px 10px 0 14px" }}>
+                                        <Text >Total pour 3 nuits</Text>
+                                        <Text >Taxes de sejour</Text>
+                                        <Text h3>Total</Text>
+
+                                    </Col>
+
+                                    <Col css={{ padding: "14px 10px 0 14px" }}>
+                                        <Text>1700 DH</Text>
+                                        <Text>230 DH</Text>
+                                        <Text>2000 DH</Text>
+                                        {/* <IoChevronDownOutline></IoChevronDownOutline> */}
+                                    </Col>
+                                </Row>
+                            </Text>
+                            {/* button de DISPONIBILITÉ */}
+                            <Row css={{ margin: "20px 0 0 0" }}>
+                                <Text css={{ padding: "30px", width: "100%", background: "rgb(179, 48, 48)", color: "rgb(243, 243, 243)" }}>
+                                    VÉRIFIER LA DISPONIBILITÉ
+                                </Text>
+                            </Row>
+
+                        </Col>
+                    </Grid>
+
+
+                </Grid.Container>
+
+
+            </Row>
+
+            <div>
+                <Modal noPadding open={visible} onClose={closeHandler} width="60%" height="100%" >
+                    <Modal.Header
+                        css={{ position: "absolute", zIndex: "$1", top: 5, right: 8 }}
+                    >
+                    </Modal.Header>
+                    <Row >
+
+
+                        {prev && <Row onClick={() => { suiverImage("prev") }} css={{ position: "absolute", zIndex: "1", top: "45%", left: "0%", cursor: "pointer" }} >
+                            <BiChevronLeft size="100px" color="white" />
+                        </Row>}
+                        <Modal.Body height="100%" width="100%" css={{ position: "relative" }}>
+                            <Image
+                                showSkeleton
+                                src={model}
+                                width="100%"
+                                height="100%"
+                            />
+                            {/* <Row css={{ display: "flex", justifyContent: "space-around", alignItems: "center" }}> */}
+
+                            {/* </Row> */}
+                        </Modal.Body>
+                        {next && <Row onClick={() => { suiverImage("next") }} css={{ position: "absolute", zIndex: "3", top: "45%", right: "-89%", cursor: "pointer" }} >
+                            <BiChevronRight color="white" size="100px" />
+                        </Row>}
+                    </Row>
+                </Modal>
+            </div>
+
+
+        </Container >
+    )
+}
+
+export default Hotel
