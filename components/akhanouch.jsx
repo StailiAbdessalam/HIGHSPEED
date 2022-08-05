fafb2d967fc8e514c1c2321e74189cbee918c9e2
BENJY33X <abdoycode@gmail.com>
2022-08-05 15:14:47 +0100

    components akhanouch

---
+++ components/akhanouch.jsx
@@ -0,0 +1,19 @@
+import { Col, Spacer, Text } from '@nextui-org/react'
+import React from 'react'
+
+const akhanouch = () => {
+    return (
+        <Col>
+            <Spacer y={1}/>
+            <Text size={20} color='#A4353A'>
+                AZIZ AKHANOUCH
+            </Text>
+            <Text size={17} color='#B05B5E'>
+                CHEF DE GOUVERMENT
+                DU MAROC
+            </Text>
+        </Col>
+    )
+}
+
+export default akhanouch
