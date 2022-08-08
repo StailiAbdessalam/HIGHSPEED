974523228bfdaf0c42772cb8f08b4121cca030b2
BENJY33X <abdoycode@gmail.com>
2022-08-08 09:50:05 +0100

    page programme

--- pages/Programme.jsx
+++ pages/Programme.jsx
@@ -9,9 +9,11 @@ const Programme = () => {
     return (
         <Row css={{ width: "100%" }}>
 
-            <Grid.Container>
-                <Grid xs={12} md={3}>
-                    <Spacer x={2} />
+            <Grid.Container >
+                {/* filter catégorie and Thematique */}
+
+                <Grid xs={12} md={4}>
+                    <Spacer x={4} />
                     <Col >
                         <Spacer y={2} />
                         <Row>
@@ -21,9 +23,9 @@ const Programme = () => {
                         </Row>
                         <Spacer y={2} />
                         <Grid.Container>
+                            {/* <Spacer y={2} /> */}
+                            <Grid xs={12} sm={6} lg={12}>
                             <Text h6 size={16}>Catégorie</Text>
-                            <Spacer y={2} />
-                            <Grid xs={6} md={12}>
                                 <Spacer x={2} />
                                 <Row css={{ width: "100%" }} >
                                     <Checkbox.Group
@@ -41,9 +43,9 @@ const Programme = () => {
                                     </Checkbox.Group>
                                 </Row>
                             </Grid>
+                            {/* <Spacer y={2} /> */}
+                            <Grid xs={12} sm={6} lg={12}>
                             <Text h6 size={16} >Thématique</Text>
-                            <Spacer y={2} />
-                            <Grid xs={6} md={12}>
                                 <Spacer x={2} />
                                 <Row>
                                     <Checkbox.Group
@@ -66,9 +68,14 @@ const Programme = () => {
                         </Grid.Container>
                     </Col>
                 </Grid>
-                <Grid xs={12} sm={9} >
+
+
+
+
+                <Grid xs={12} md={15 / 2} >
                     <Col css={{ width: "100%" }}>
-                        <Col css={{ width: "100%", backgroundColor: "#FAFAFA", padding: "20px" }}>
+                        {/* partie one  */}
+                        <Col css={{ width: "100%", backgroundColor: "#FAFAFA", padding: "20px 80px" }}>
                             <Spacer y={2} />
                             <Text color='#5B3F3F' size={18} h3>8:00 AM - 9:00 AM EST (60mins)</Text>
                             <Spacer x={1} />
@@ -99,14 +106,14 @@ const Programme = () => {
                                 </Row>
                             </Col>
                         </Col>
+
                         <Row>
                         </Row>
                         <Spacer y={2} />
-
-
-                        <Col css={{ backgroundColor: "#DA3840", padding: "20px" }}>
+                        {/* partie two */}
+                        <Col css={{ backgroundColor: "#DA3840", padding: "20px 80px" }}>
+                            <Spacer y={2} />
                             <Text color='#FAFAFA' size={18} h3>8:00 AM - 9:00 AM EST (60mins)</Text>
-
                             <Spacer x={1} />
                             <Row >
                                 <Text color='#FAFAFA' h1 size={34}>
@@ -129,37 +136,50 @@ const Programme = () => {
                                 </Text>
                             </Col>
                         </Col>
-                        <Row css={{ backgroundColor: "#D9D9D9", padding: "40px 20px" }}>
-                            <Col>
-                                <Row >
-                                    <Text color='#DA3F47' h3 size={20}>SPEACKERS</Text>
-                                </Row>
-                                <Row>
-                                    <Akhanouch />
-                                    <Akhanouch />
-                                </Row>
-                                <Row>
-                                    <Akhanouch />
-                                    <Akhanouch />
-                                </Row>
+                        <Row css={{ backgroundColor: "#D9D9D9", padding: "40px 80px" }}>
+
+                            <Grid.Container gap={2}>
+                                <Grid xs={12} md={9} >
+                                    <Col>
+                                        <Row >
+                                            <Text color='#DA3F47' h3 size={20}>SPEACKERS</Text>
+                                        </Row>
+
+                                        <Row>
+                                            <Akhanouch />
+                                            <Akhanouch />
+                                        </Row>
+                                        <Row>
+                                            <Akhanouch />
+                                            <Akhanouch />
+                                        </Row>
+
+                                    </Col>
+                                </Grid>
+                                <Grid xs={12} md={3}>
+
+                                    <Col>
+                                        <Row >
+                                            <Text color='#DA3F47' h3 size={20}>MODERATEUR</Text>
+                                        </Row>
+                                        <Row>
+                                            <Akhanouch />
+                                        </Row>
+                                    </Col>
+                                </Grid>
                                 <Spacer y={3} />
                                 <Row css={{ paddingLeft: "70px" }}>
                                     <Text size={12}>
                                         Fermer
                                     </Text>
                                 </Row>
-                            </Col>
-                            <Col>
-                                <Row >
-                                    <Text color='#DA3F47' h3 size={20}>MODERATEUR</Text>
-                                </Row>
-                                <Row>
-                                    <Akhanouch />
-                                </Row>
-                            </Col>
+                            </Grid.Container>
+
+
                         </Row>
                         <Spacer y={2} />
-                        <Col css={{ width: "100%", backgroundColor: "#FAFAFA", padding: "20px" }}>
+                        {/* partie 3 */}
+                        <Col css={{ width: "100%", backgroundColor: "#FAFAFA", padding: "20px 80px" }}>
                             <Spacer y={2} />
                             <Text color='#5B3F3F' size={18} h3>8:00 AM - 9:00 AM EST (60mins)</Text>
                             <Spacer x={1} />
@@ -190,20 +210,63 @@ const Programme = () => {
                                 </Row>
                             </Col>
                         </Col>
-
-                        <Col>
+                        <Row></Row>
+                        <Spacer y={2} />
+                        {/* partie green 4 */}
+                        <Col css={{ width: "100%", backgroundColor: "#EEFAE5", padding: "20px 80px 90px 80px" }}>
+                            <Spacer y={2} />
                             <Col>
-                                <Row></Row>
-                                <Row></Row>
-                                <Row></Row>
+                                <Row>
+                                    <Text color='#1B7F6B' size={18} h3>8:00 AM - 9:00 AM EST (60mins)</Text>
+                                </Row>
+                                <Spacer y={3} />
+                                <Row>
+                                    <Text color='#064030' weight={'extrabold'} h1 size={50}>BREAK </Text>
+                                </Row>
+                                <Spacer y={3} />
+                                <Row>
+                                    <Text css={{ backgroundColor: "#83BD56", padding: "10px 30px" }}>Coffee</Text>
+                                    <Spacer x={1} />
+                                    <Text css={{ backgroundColor: "#83BD56", padding: "10px 30px" }}>Buffet</Text>
+                                </Row>
                             </Col>
                             <Row>
-
                             </Row>
                         </Col>
-
-
-
+                        <Row></Row>
+                        <Spacer y={2} />
+                        {/* parite final */}
+                        <Col css={{ width: "100%", backgroundColor: "#FAFAFA", padding: "20px 80px" }}>
+                            <Spacer y={2} />
+                            <Text color='#5B3F3F' size={18} h3>8:00 AM - 9:00 AM EST (60mins)</Text>
+                            <Spacer x={1} />
+                            <Row >
+                                <Text color='#422222' h1 size={34}>
+                                    Opening Keynote : <br />
+                                    Persisting in the Fight for change
+                                </Text>
+                            </Row>
+                            <Spacer x={1} />
+                            <Col>
+                                <Text color='#AEADAD'> Session Pieniere</Text>
+                                <Spacer x={1} />
+                                <Row>
+                                    <Text css={{ backgroundColor: "$red200", width: "250px", padding: "7px" }} >THE FILLMORE - MAIN STAGE</Text>
+                                    <Spacer x={3} />
+                                    <Row css={{ backgroundColor: "$red200", width: "250px", padding: "7px" }}>LEADERSHIP INSIGHTS</Row>
+                                </Row>
+                                <Spacer x={1} />
+                                <Text color='#8C7979'>
+                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex alias molestias, eius, hic est similique ducimus itaque maiores, quos delectus maxime. Laboriosam ad ullam illum consectetur eos officia facilis numquam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, inventore?
+                                </Text>
+                                <Spacer y={2} />
+                                <Row css={{ paddingLeft: "70px" }}>
+                                    <Text size={12}>
+                                        En savoir plus
+                                    </Text>
+                                </Row>
+                            </Col>
+                        </Col>
                     </Col>
                 </Grid>
             </Grid.Container>
