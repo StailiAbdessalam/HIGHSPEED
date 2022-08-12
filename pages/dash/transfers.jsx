944513c2e05cdd7fc0b25f9384ae4536f0e0f4d5
BENJY33X <abdoycode@gmail.com>
2022-08-12 13:22:52 +0100

    transfers is the page of reserver the transport

---
+++ pages/dash/transfers.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const transfers = () => {
+
+  const transfers = [
+    {
+      from : "New York",
+      to : "New York",
+      price : "100",
+      trransportType : "car",
+    }
+  ]
+  return (
+    <div>transfers</div>
+  )
+}
+
+export default transfers
