bd3320497c2ecfd3cf7ff698d4db998234ecc6e5
BENJY33X <abdoycode@gmail.com>
2022-08-12 13:22:09 +0100

    file of data vertual

---
+++ pages/dash/data.js
@@ -0,0 +1,88 @@
+
+export const accomdition = [
+    {
+        id: 1,
+        name: "Oscar ***",
+        image: "https://images.pexels.com/photos/594077/pexels-photo-594077.jpeg?auto=compress&cs=tinysrgb&w=1600",
+        price: "$100",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        checkin: "12:00",
+        checkout: "12:00",
+        rating: "4.5",
+        location: "New York",
+        stars: "4",
+    },
+    {
+        id: 2,
+        name: "Sofitel",
+        image: "https://images.pexels.com/photos/258154/pexels-photo-258154.jpeg?auto=compress&cs=tinysrgb&w=1600",
+        price: "$100",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        checkin: "12:00",
+        checkout: "12:00",
+        rating: "4.5",
+        location: "New York",
+        stars: "4",
+    },
+    {
+        id: 3,
+        name: "Sofitel",
+        image: "https://images.pexels.com/photos/97904/pexels-photo-97904.jpeg?auto=compress&cs=tinysrgb&w=1600",
+        price: "$100",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        checkin: "12:00",
+        checkout: "12:00",
+        rating: "4.5",
+        location: "New York",
+        stars: "4",
+    },
+]
+
+export const activates = [
+    {
+        name: "NAme of the activity",
+        start: "21:00 PM",
+        end: "00:00 PM",
+        price: "100",
+        image: "https://images.pexels.com/photos/2422264/pexels-photo-2422264.jpeg?auto=compress&cs=tinysrgb&w=1600"
+    },
+    {
+        name: "NAme of the activity",
+        start: "07:00 PM",
+        end: "12:00 PM",
+        price: "100",
+        image: "https://images.pexels.com/photos/6592700/pexels-photo-6592700.jpeg?auto=compress&cs=tinysrgb&w=1600"
+    }
+]
+
+export const transport = [
+    {
+      title: "car rental",
+      img: "https://images.pexels.com/photos/112460/pexels-photo-112460.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      price: "$5.50",
+      from : "New York",
+      to : "New York",
+    },
+    {
+      title: "car class",
+      img: "https://images.pexels.com/photos/712618/pexels-photo-712618.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      price: "$3.00",
+      from : "New York",
+      to : "New York",
+
+    },
+    {
+      title: "Raspberry",
+      img: "https://images.pexels.com/photos/164654/pexels-photo-164654.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      price: "$10.00",
+      from : "New York",
+      to : "New York",
+    },
+    {
+      title: "bus",
+      img: "https://images.pexels.com/photos/6156582/pexels-photo-6156582.jpeg?auto=compress&cs=tinysrgb&w=1600",
+      price: "$2.30",
+      from : "New York",
+      to : "New York",
+    }
+  ];
